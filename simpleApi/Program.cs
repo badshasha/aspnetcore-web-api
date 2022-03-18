d5dc8f5b9788560a9441d82edafecd372c515c50
badshasha <shavendragoesoft@gmail.com>
2022-03-17 22:14:04 -0700

    seed info watch AppDbInitializer and progam.cs

--- simpleApi/Program.cs
+++ simpleApi/Program.cs
@@ -31,4 +31,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+AppDbInitializer.Seed(app);
+
 app.Run();
---
+++ simpleApi/data/AppDbInitializer.cs
@@ -0,0 +1,32 @@
+﻿namespace simpleApi.data
+{
+    public class AppDbInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder) {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope()) 
+            { 
+                var context = serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
+                if (!context.books.Any()) 
+                {
+                    context.books.AddRange(
+                        new Book
+                        {
+                            Title = "1 book",
+                            Desctiption = "1 book Deskctioption",
+                            IsRead = true,
+                            ReadDate = DateTime.Now.AddDays(-10)
+                        },
+                        new Book {
+                            Title = "2 book",
+                            Desctiption = "2 book Deskctioption",
+                            IsRead = false,                           
+                        }
+                        ) ;
+
+                    context.SaveChanges();
+                 
+                }
+            }
+        }
+    }
+}
